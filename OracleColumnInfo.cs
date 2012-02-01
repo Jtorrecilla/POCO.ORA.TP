7b25396715d020f1366f109be4baa4eb8e888ab3
JTorrecilla <rastagar2003@yahoo.es>
2012-02-01 12:57:58 +0100

    Update Schemma

---
+++ OracleColumnInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POCO.Ora.TP
+{
+    public class OracleColumnInfo
+    {
+        public OracleColumnInfo()
+        {
+            IsPk = false; 
+            Nullable = false;
+        }
+        public bool IsPk { get; set; }
+        public string ColumnName { get; set; }
+        public bool Nullable { get; set; }
+        public string DefaultValue { get; set; }
+        public string SequenceName { get; set; }
+    }
+
+}
